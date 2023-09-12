5cd48be701cfb5198f2f9be158a6fae975268ee6
Daniel Riverol <127858166+DanielRiverol82@users.noreply.github.com>
2023-09-12 11:46:31 -0300

    Proyecto final

---
+++ js/camera.js
@@ -0,0 +1,41 @@
+//clase anterior
+const botonAbrirCamara = document.querySelector("[data-video-boton]");
+const campoCamara = document.querySelector("[data-camera]");
+const video = document.querySelector("[data-video]");
+
+const botonTomarFoto = document.querySelector("[data-tomar-foto]");
+const canvas = document.querySelector("[data-video-canvas]");
+const mensaje = document.querySelector("[data-mensaje]");
+
+let imagenURL = "";
+//
+const botonEnviar = document.querySelector("[data-enviar]");
+
+//codigo anterior
+botonAbrirCamara.addEventListener("click", async () => {
+  const iniciarVideo = await navigator.mediaDevices.getUserMedia({
+    video: true,
+    audio: false,
+  });
+  botonAbrirCamara.style.display = "none";
+  campoCamara.style.display = "block";
+  video.srcObject = iniciarVideo;
+});
+
+//Evento guardar
+botonTomarFoto.addEventListener("click", () => {
+  canvas.getContext("2d").drawImage(video, 0, 0, canvas.width, canvas.height);
+  imagenURL = canvas.toDataURL("image/jpeg");
+  campoCamara.style.display = "none";
+  mensaje.style.display = "block";
+});
+
+//
+botonEnviar.addEventListener("click", () => {
+  const recibirDatosGuardados = localStorage.getItem("registro");
+  const convertirDatos = JSON.parse(recibirDatosGuardados);
+  convertirDatos.img_url = imagenURL;
+  localStorage.setItem("registro", JSON.stringify(convertirDatos));
+
+  window.location.href = "./abrir-cuenta-form-3.html";
+});
