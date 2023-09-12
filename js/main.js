36d55a6caab4cbecd0f26e5c8a23b9e1abd694f1
Daniel Riverol <127858166+DanielRiverol82@users.noreply.github.com>
2023-09-12 14:54:57 -0300

    Actualizando funciones

--- js/main.js
+++ js/main.js
@@ -1,16 +1,13 @@
-/* paso 2 importar validar-cuit */
 import esUnCUIT from "./validar-cuit.js";
 
 const camposDeFormulario = document.querySelectorAll("[required");
 
-/* console.log(camposDeFormulario); */
 
 camposDeFormulario.forEach((campo) => {
   campo.addEventListener("blur", () => verificarCampo(campo));
 });
 
 function verificarCampo(campo) {
-  /* paso 3 crear condicional y llamar a la funcion y pasarle el campo */
   if (campo.name == "cuil" && campo.value.length >= 11) {
     esUnCUIT(campo);
   }
